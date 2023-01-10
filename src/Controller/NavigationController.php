d6a2a203335cc32a9e02f47848360f5f5949fbf0
PaulDlcr <delacrepaul@gmail.com>
2023-01-10 14:54:11 +0100

    :white_check_mark: + Sign Up & Log In

---
+++ src/Controller/NavigationController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+class NavigationController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index()
+    {
+        return $this->render('navigation/home.html.twig');
+    }
+
+    #[Route('/home', name: 'home')]
+    public function membre()
+    {
+        $hasAccess = $this->isGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        return $this->render('home/home.html.twig');
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function admin(Session $session)
+    {
+        $user = $this->getUser();
+
+        if($user && in_array('ROLE_ADMIN', $user->getRoles())){
+            return $this->render('navigation/admin.html.twig');
+        } 
+
+        $session->set("message", "Vous n'avez pas le droit d'acceder à la page admin vous avez été redirigé sur cette page");
+        return $this->renderToRoute('home');
+    }
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,85 +3,87 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
+#[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'create_user')]
-    public function createUser(ManagerRegistry $doctrine):Response
-    {  	
-        $entityManager = $doctrine->getManager();
-
-        $user = new User();
-        $user->setEmail('pd@gmail.com');
-        $user->setUsername('pold');
-        $user->setPassword('test');
-
-        $entityManager->persist($user);
-
-        $entityManager->flush();
-
-        return new Response('Saved new user with id '.$user->getId());
-    }
-
-    #[Route('/user/{id}', name: 'user_show')]
-    public function show(ManagerRegistry $doctrine, int $id): Response
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
     {
-        $user = $doctrine->getRepository(User::class)->find($id);
-
-        if (!$user) {
-            throw $this->createNotFoundException(
-                'No user found for id '.$id
-            );
-        }
-
-        return new Response($user->getUsername().' Found ! ');
-
-        // or render a template
-        // in the template, print things with {{ product.name }}
-        // return $this->render('product/show.html.twig', ['product' => $product]);
-    }
-
-    #[Route('/user/edit/{ }', name: 'user_edit')]
-    public function update(ManagerRegistry $doctrine, int $id): Response
-    {
-        $entityManager = $doctrine->getManager();
-        $user = $entityManager->getRepository(User::class)->find($id);
-
-        if (!$user) {
-            throw $this->createNotFoundException(
-                'No user found for id '.$id
-            );
-        }
-
-        $user->SetUsername('test');
-        $entityManager->flush();
-
-        return $this->redirectToRoute('user_show', [
-            'id' => $user->getId()
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
         ]);
     }
-    #[Route('/user/delete/{id}', name: 'user_delete')]
-    public function delete(ManagerRegistry $doctrine, int $id): Response
-    {
-        $entityManager = $doctrine->getManager();
-        $user = $entityManager->getRepository(User::class)->find($id);
 
-        if (!$user) {
-            throw $this->createNotFoundException(
-                'No user found for id '.$id
-            );
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager = $doctrine->getManager();
+                //encodage du mot de passe
+                $user->setPassword(
+                    $passwordHasher->hashPassword($user, $user->getPassword())
+                );
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_user_index');
         }
 
-        $entityManager->remove($user);
-        $entityManager->flush();
-
-        return new Response('Deleted user! ');
-
+        return $this->render('user/new.html.twig', [
+        'user' => $user,
+        'form' => $form->createView(),
+        ]);
     }
 
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($user, true);
+        }
+
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
